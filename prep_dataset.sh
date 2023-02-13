4e2eaabcade8e32033faa5dbe2927096942e869c
Mike Mannion <mike.mannion@rnfdigital.com>
2023-02-13 21:27:59 +0100

    Hyperparameter variations (learning rate)... have zero impact on accuracy, which is rubbish.

--- prep_dataset.sh
+++ prep_dataset.sh
@@ -6,7 +6,6 @@ cp mfc_dataset/female_1???.jpg mfc_dataset_train_test/train/female
 cp mfc_dataset/female_2???.jpg mfc_dataset_train_test/train/female
 cp mfc_dataset/female_3???.jpg mfc_dataset_train_test/train/female
 cp mfc_dataset/female_4???.jpg mfc_dataset_train_test/train/female
-cp mfc_dataset/female_5???.jpg mfc_dataset_train_test/test/female
 cp mfc_dataset/male_?.jpg mfc_dataset_train_test/train/male
 cp mfc_dataset/male_??.jpg mfc_dataset_train_test/train/male
 cp mfc_dataset/male_???.jpg mfc_dataset_train_test/train/male
@@ -14,4 +13,12 @@ cp mfc_dataset/male_1???.jpg mfc_dataset_train_test/train/male
 cp mfc_dataset/male_2???.jpg mfc_dataset_train_test/train/male
 cp mfc_dataset/male_3???.jpg mfc_dataset_train_test/train/male
 cp mfc_dataset/male_4???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_6???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_7???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_8???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_9???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_1????.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_2????.jpg mfc_dataset_train_test/train/male
+
+cp mfc_dataset/female_5???.jpg mfc_dataset_train_test/test/female
 cp mfc_dataset/male_5???.jpg mfc_dataset_train_test/test/male
