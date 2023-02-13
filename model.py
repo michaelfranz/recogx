5340ba72167a27cfed031514b8920febe4ff5252
Mike Mannion <mike.mannion@rnfdigital.com>
2023-02-13 18:48:34 +0100

    Now retains color channel and processes accordingly. Accuracy is rubbish.

--- model.py
+++ model.py
@@ -2,23 +2,24 @@
 # functions. The model takes in Mel-frequency cepstrum (MFC) images of size 98x12 and outputs a binary classification
 # (male or female).
 
-import torch
 import torch.nn as nn
-
 
 class GenderClassifier(nn.Module):
     def __init__(self):
         super(GenderClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.pool = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(1472, 128)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64 * 24 * 3, 128)
         self.fc2 = nn.Linear(128, 2)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = x.view(-1, 1472)
-        x = torch.relu(self.fc1(x))
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(-1, 64 * 24 * 3)
+        x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
---
+++ prep_dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+cp mfc_dataset/female_?.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_??.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_???.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_1???.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_2???.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_3???.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_4???.jpg mfc_dataset_train_test/train/female
+cp mfc_dataset/female_5???.jpg mfc_dataset_train_test/test/female
+cp mfc_dataset/male_?.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_??.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_1???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_2???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_3???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_4???.jpg mfc_dataset_train_test/train/male
+cp mfc_dataset/male_5???.jpg mfc_dataset_train_test/test/male
